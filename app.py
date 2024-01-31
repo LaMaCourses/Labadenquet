e5acad6fcfabb91b767b4cddea351ffc5105a812
LaMa <43755834+LaMaCourses@users.noreply.github.com>
2024-01-31 21:58:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-variable_a_modifier = None
+variable = None
 
 @app.route('/')
 def index():
@@ -9,17 +9,15 @@ def index():
 
 @app.route('/modifier_variable', methods=['POST'])
 def modifier_variable():
-    global variable_a_modifier
-    bouton_presse = request.form.get('bouton_presse')
-    # Logique pour modifier la variable en fonction du bouton pressé
-    variable_a_modifier = bouton_presse
+    global variable
+    variable = request.form.get('bouton')
     return jsonify({'success': True})
 
 @app.route('/consulter_variable', methods=['GET'])
 def consulter_variable():
-    global variable_a_modifier
+    global variable
     # Logique pour consulter la variable
-    return jsonify({'variable': variable_a_modifier})
+    return jsonify({'variable': variable})
 
 if __name__ == '__main__':
     app.run(debug=True)
